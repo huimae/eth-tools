9350897caafb6f909fdf7ce1a5ac8defb99a095a
奶爸 <hi@nai.ba>
2019-07-17 15:00:06 +0800

    Update build script

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-xgo --targets=windows/amd64 --ldflags="-H=windowsgui -extldflags=-s" --pkg github.com/naiba/hunter -v .
+xgo --targets=windows/amd64 --ldflags="-H=windowsgui -extldflags=-s" --pkg github.com/naiba/hunter -v . &&
+    xgo --targets=darwin/amd64 --ldflags="-extldflags=-s" --pkg github.com/naiba/hunter -v .
