c5462def7c4e883ec60d9d79d4ebf653fbb3cb9f
奶爸 <hi@nai.ba>
2019-07-26 23:45:38 +0800

    优化

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ var networks = []string{
 }
 
 func setupUI() {
-	mainwin := ui.NewWindow("Token 领取器", 400, 50, true)
+	mainwin := ui.NewWindow("ETH 调试工具", 300, 418, true)
 	mainwin.OnClosing(func(*ui.Window) bool {
 		ui.Quit()
 		return true
@@ -46,6 +46,7 @@ func setupUI() {
 	mainBox := ui.NewVerticalBox()
 	mainBox.SetPadded(true)
 
+	// ============= Choose Network =============
 	networkBox := ui.NewHorizontalBox()
 	networkBox.SetPadded(true)
 	networkLabel := ui.NewLabel("选择节点")
@@ -55,14 +56,14 @@ func setupUI() {
 	}
 	networkBox.Append(networkLabel, false)
 	networkBox.Append(networkCombo, true)
-
-	tokenEntry, tokenBox := uiutil.GetEntry("代币地址")
+	// ============= Set Token Info =============
 	walletEntry, walletBox := uiutil.GetEntry("钱包地址")
 	numEntry, numBox := uiutil.GetEntry("领取数量")
 
-	getBox := ui.NewHorizontalBox()
-	getBox.SetPadded(true)
-
+	// ============= Get Token =============
+	getTokenBox := ui.NewVerticalBox()
+	getTokenBox.SetPadded(true)
+	tokenEntry, tokenBox := uiutil.GetEntry("代币地址")
 	getBtn := ui.NewButton("获取代币")
 	getBtn.OnClicked(func(b *ui.Button) {
 		if !b.Enabled() {
@@ -73,8 +74,28 @@ func setupUI() {
 		network := strings.Split(networks[networkCombo.Selected()], "#")
 		go getToken(false, "01491231C2C71D99A16C7FB2120E185EAAE0861548B5FBF971859099DAB5DCA2", tokenEntry.Text(), walletEntry.Text(), network[1], num, b, mainwin)
 	})
-	getBox.Append(getBtn, true)
+	getTokenBox.Append(tokenBox, true)
+	getTokenBox.Append(getBtn, true)
 
+	// ============= Get ETH =============
+	getETHBox := ui.NewVerticalBox()
+	getETHBox.SetPadded(true)
+
+	qiongbiBtn := ui.NewButton("穷逼领钱")
+	qiongbiBtn.OnClicked(func(b *ui.Button) {
+		if !b.Enabled() {
+			return
+		}
+		num, _ := strconv.ParseInt(numEntry.Text(), 10, 64)
+		if num > 10 || num < 1 {
+			ui.MsgBox(mainwin, "数量错误", "数量在 1 到 100 之间")
+			return
+		}
+		b.Disable()
+		network := strings.Split(networks[networkCombo.Selected()], "#")
+		go getToken(true, "01491231C2C71D99A16C7FB2120E185EAAE0861548B5FBF971859099DAB5DCA2", tokenEntry.Text(), walletEntry.Text(), network[1], num, b, mainwin)
+	})
+	getETHBox.Append(qiongbiBtn, true)
 	chargeBtn := ui.NewButton("充值ETH")
 	chargeBtn.OnClicked(func(b *ui.Button) {
 		if !b.Enabled() {
@@ -83,25 +104,31 @@ func setupUI() {
 		b.Disable()
 		go charge(b, mainwin)
 	})
-	getBox.Append(chargeBtn, true)
+	getETHBox.Append(chargeBtn, true)
 
-	qiongbiBtn := ui.NewButton("穷逼领钱")
-	qiongbiBtn.OnClicked(func(b *ui.Button) {
-		if !b.Enabled() {
-			return
-		}
-		b.Disable()
-		num, _ := strconv.ParseInt(numEntry.Text(), 10, 64)
-		network := strings.Split(networks[networkCombo.Selected()], "#")
-		go getToken(true, "01491231C2C71D99A16C7FB2120E185EAAE0861548B5FBF971859099DAB5DCA2", tokenEntry.Text(), walletEntry.Text(), network[1], num, b, mainwin)
-	})
-	getBox.Append(qiongbiBtn, true)
+	// ============= A Tab =============
+	mainTab := ui.NewTab()
 
-	mainBox.Append(networkBox, false)
-	mainBox.Append(tokenBox, false)
-	mainBox.Append(numBox, false)
-	mainBox.Append(walletBox, false)
-	mainBox.Append(getBox, false)
+	// ============= Tips Tab =============
+	tipsLb := ui.NewMultilineEntry()
+	tipsLb.SetReadOnly(true)
+	tipsLb.SetText("前置操作：\n" +
+		"1.填写你的钱包地址\n" +
+		"2.填写需要领取的数量（领取代币无限制，ETH 限额为 1-100）\n" +
+		"领取代币：\n" +
+		"1.填写代币地址\n" +
+		"领取ETH：\n" +
+		"1.点击「穷逼领钱」按钮\n" +
+		"2.如果领取不成功点击「充值ETH」充值库存",
+	)
+
+	mainBox.Append(networkBox, false) //选择网络
+	mainBox.Append(walletBox, false)  // 钱包地址
+	mainBox.Append(numBox, false)     // 设置数量
+	mainTab.Append("获取代币", getTokenBox)
+	mainTab.Append("获取ETH", getETHBox)
+	mainBox.Append(mainTab, false) // 领取 ETH 或 代币
+	mainBox.Append(tipsLb, true)   // 使用说明
 	mainwin.SetChild(mainBox)
 	mainwin.Show()
 }
@@ -140,34 +167,31 @@ func getToken(isETH bool, pk, tokenAddr, walletAddr, network string, num int64, 
 	TBCAdminPub := crypto.PubkeyToAddress(*publicKeyECDSA)
 
 	if isETH {
-
-		// SEND ETH == Start
 		opt := ethutil.GenerateTransactOpts(client, TBCAdminPk, TBCAdminPub)
-		opt.Value = big.NewInt(1000000000000000) // in wei (1 eth)
+		opt.Value = big.NewInt(num * 10000000000000) // in wei (1 eth)
 		var data []byte
 		tx := types.NewTransaction(opt.Nonce.Uint64(), common.HexToAddress(walletAddr), opt.Value, opt.GasLimit, opt.GasPrice, data)
 
 		chainID, err := client.NetworkID(context.Background())
 		if err != nil {
-			setTitle(fmt.Sprint("获取失败 %s", err))
+			setTitle(fmt.Sprintf("获取失败 %s", err))
 			return
 		}
 
 		setTitle("Transcation 签名")
 		signedTx, err := types.SignTx(tx, types.NewEIP155Signer(chainID), TBCAdminPk)
 		if err != nil {
-			setTitle(fmt.Sprint("获取失败 %s", err))
+			setTitle(fmt.Sprintf("获取失败 %s", err))
 			return
 		}
 
 		setTitle("广播 Transcation")
 		err = client.SendTransaction(context.Background(), signedTx)
 		if err != nil {
-			setTitle(fmt.Sprint("获取失败 %s", err))
+			setTitle(fmt.Sprintf("获取失败 %s", err))
 			return
 		}
 		setTitle("发送成功")
-		// SEND ETH == END
 	} else {
 		token, err := erc20.NewErc20(common.HexToAddress(tokenAddr), client)
 		if err != nil {
