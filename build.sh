c58115b59c040aabe599bee5332e2f74ea43adac
奶爸 <hi@nai.ba>
2019-08-01 20:13:17 +0800

    Min size

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-xgo --targets=windows/amd64 --ldflags="-H=windowsgui -extldflags=-s" --pkg github.com/naiba/eth-debugger -v . &&
+rm -rf eth-debugger-* && xgo --targets=windows/amd64 --ldflags="-s -w -H=windowsgui -extldflags=-static" --pkg github.com/naiba/eth-debugger -v . &&
     xgo --targets=darwin/amd64 --ldflags="-extldflags=-s" --pkg github.com/naiba/eth-debugger -v .
